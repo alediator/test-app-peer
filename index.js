05cf862f331ca507f4767f3f173c976fca93efe1
Diaz Torres, Alejandro <alejandro.diaztorres@aareon.com>
2021-04-22 08:09:12 +0200

    Add debug

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { PeerServer } = require('peer');
 
 const port = process.env.NODE_PORT ? process.env.NODE_PORT : (process.env.PORT ? process.env.PORT : 3001);
+const debug = process.env.DEBUG ? process.env.DEBUG : true;
 
-console.log('Running peerjs in ' + port);
-const peerServer = PeerServer({ port: port, path: '/' });
+console.log('Running peerjs in ', + port, ' debug=', debug);
+const peerServer = PeerServer({ 
+    port: port, 
+    path: '/',
+    debug: debug
+});
